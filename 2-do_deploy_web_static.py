6ed0cf8f115af9f80bb5d543ad198125ab273b7a
peterodero561 <peterodero561@gmail.com>
2024-05-15 07:42:09 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:'''
+'''a Fabric script (based on the file 1-pack_web_static.py)
+    that distributes an archive to your web servers,
+    using the function do_deploy:'''
 
 from fabric.api import run, put, env
 import os
@@ -9,6 +11,8 @@ from os.path import exists
 # Define the list of web servers
 env.hosts = ['54.162.83.217', '3.85.168.57']
 env.user = 'ubuntu'
+
+
 def do_deploy(archive_path):
     """
     Distributes an archive to web servers and deploys it.
@@ -21,11 +25,13 @@ def do_deploy(archive_path):
         # Upload the archive to /tmp/ directory of the web server
         put(archive_path, '/tmp/')
 
-        # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
+        # Extract the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
         filename = archive_path.split('/')[-1]
         folder_name = '/data/web_static/releases/{}'.format(filename[:-4])
         run('mkdir -p {}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C {} --strip-components=1'.format(filename, folder_name))
+        run('tar -xzf /tmp/{} -C {} --strip-components=1'.format(
+            filename, folder_name))
 
         # Delete the archive from the web server
         run('rm /tmp/{}'.format(filename))
