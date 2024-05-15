b268de908f00474920cae895c4775e5600667f05
peterodero561 <peterodero561@gmail.com>
2024-05-15 06:55:25 +0000

    perf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,7 +28,7 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i "/server_name peterdev.tech;/a \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo systemctl restart nginx
