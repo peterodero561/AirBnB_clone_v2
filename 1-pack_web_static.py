7367d904e424dd33140a5418a1d5a29325c40486
peterodero561 <peterodero561@gmail.com>
2024-05-15 06:00:43 +0000

    fix:corrected PEP8 errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-''' Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.'''
+''' Fabric script that generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone repo,
+    using the function do_pack.'''
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """
